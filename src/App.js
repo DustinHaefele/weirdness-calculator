84bbc5e7db040d256441c2876bd434dd9259f834
Dustin Haefele <Dustin.haefele@gmail.com>
2019-11-24 12:36:05 -0300

    giphy api setup: created giphy services object

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './App.css';
-import config from './config';
 import Header from './components/Header/Header';
 
 function App() {
---
+++ src/services/giphy-api-service.js
@@ -0,0 +1,16 @@
+import config from './config';
+
+const GiphyApiService = {
+  getGifFromSearch(searchTerm, weirdness) {
+    return fetch(
+      `${config.API_ENDPOINT}?api_key=${config.API_KEY}&s=${searchTerm}&weirdness=${weirdness}`,
+      {
+        method: 'GET'
+      }
+    ).then(res =>
+      !res.ok ? res.json().then(e => Promise.reject(e)) : res.json()
+    );
+  },
+};
+
+export default GiphyApiService;
