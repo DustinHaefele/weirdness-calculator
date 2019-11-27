5b11ec296e386d4fdca266c644b9ad2ca9a03a53
Dustin Haefele <Dustin.haefele@gmail.com>
2019-11-27 20:53:13 -0300

    removeFavorite: Added "x" at the top of favorite gif and functionality to remove it onClick

--- src/components/Favorites/Favorites.js
+++ src/components/Favorites/Favorites.js
@@ -1,23 +1,24 @@
 import React from 'react';
-import { removeFromFavorites } from '../../redux/actions';
 import GifDisplay from '../GifDisplay/GifDisplay';
+import './Favorites.css';
 
 export default function Favorites({ favorites, dispatch }) {
   
-  function handleRemove(id) {
-    dispatch(removeFromFavorites(id))
-  }
-
   function displayFavorites() {
-    return favorites.map(f => <div key={f.id} onClick={()=>handleRemove(f.id)}><GifDisplay gif={f.gif}/></div>)
-  };
-
-
+    return favorites.map(f => (
+      <div key={f.id}>
+        <GifDisplay gif={f.gif} isFavorite={true} />
+      </div>
+    ));
+  }
 
   return (
     <div>
-      {(favorites.length > 0) ? displayFavorites() 
-      : <p>When you add gifs to your favorites they will be displayed here</p>}
+      {favorites.length > 0 ? (
+        displayFavorites()
+      ) : (
+        <p>When you add gifs to your favorites they will be displayed here</p>
+      )}
     </div>
   );
 }
--- src/components/GifDisplay/GifDisplay.js
+++ src/components/GifDisplay/GifDisplay.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
+import { removeFromFavorites } from '../../redux/actions';
 import { connect } from 'react-redux';
 import './GifDisplay.css';
 import ImageLoader from 'rc-image-loader';
 import GifLoader from '../GifLoader/GifLoader';
 
-
-const GifDisplay = ({ gif }) => {
+const GifDisplay = ({ gif, isFavorite = false, dispatch }) => {
   const history = useHistory();
 
   if (!gif.images) {
@@ -14,24 +14,30 @@ const GifDisplay = ({ gif }) => {
     return <></>;
   }
 
-
+  function handleRemove(id) {
+    dispatch(removeFromFavorites(id));
+  }
 
   const url = gif.images.fixed_width.url || '';
   const title = `${gif.searchTerm}: ${gif.title}` || '';
 
+  const remove = isFavorite ? 'remove' : 'hidden';
+
   return (
-    <div className='_40 around'>
-      {/*  className="gifDisplay" */}
+    <div className="_40 around">
       <h3 className="gifTitle">{title}</h3>
-      {/* <div className="imgDiv"> */}
+      <div className='wrapper'>
       <ImageLoader
-        placeholder='../../../2705-image-loading.gif'
+        placeholder="../../../2705-image-loading.gif"
         image={url}
-        renderComponent={({ src }) => (<img src={src} alt={title} />)}
-        renderLoading={() => (<GifLoader />)}
-        renderError={() => (<text>Failed to load your gif, please try again </text>)}
-    />
-      {/* </div> */}
+        renderComponent={({ src }) => <img src={src} alt={title} />}
+        renderLoading={() => <GifLoader />}
+        renderError={() => (
+          <text>Failed to load your gif, please try again </text>
+        )}
+      />
+      <span className={remove} onClick={()=>handleRemove(gif.id)}>X</span>
+      </div>
     </div>
   );
 };
