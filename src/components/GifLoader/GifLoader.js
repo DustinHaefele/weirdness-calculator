5f379c28b2845e32af91836ad03d904e753dd2b8
Dustin Haefele <Dustin.haefele@gmail.com>
2019-11-27 19:18:34 -0300

    GifLoading: Added a component for a loading animation and added a package to handle the transition on loading

---
+++ src/components/GifLoader/GifLoader.js
@@ -0,0 +1,21 @@
+import React from 'react';
+// import Lottie from 'react-lottie';
+import Lottie from 'lottie-react-web';
+import GifLoader from '../../images/gif-loading.json';
+import './GifLoader.css';
+
+export default function Spinner () {
+
+  const defaultOptions = {
+        loop: true,
+        autoplay: true,
+        animationData: GifLoader,
+      };
+  
+    return (
+      <div className='spinner-div' >
+        <Lottie className='lottie-player' options={defaultOptions}/>
+        <p>Give us just a second and we'll have your gif ready for you!</p>
+      </div>
+    )
+  }
